5dd4fc12df186a026b9df405be5413c9dc041a93
APTXZwei <104445134+APTXZwei@users.noreply.github.com>
2023-09-13 02:04:42 +0000

    base setup

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 import type { AppInfo } from '@/types/app'
-export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
-export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
+export const APP_ID = '3fc28eb5-7f0e-471a-b50d-1e89a2f53844'
+export const API_KEY = 'app-sQ3ebsceQscNMpmUjEyNnrv0'
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
   title: 'Chat APP',
